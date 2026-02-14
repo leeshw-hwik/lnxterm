cd14232799dcca99ce77885d392545b21c731913
leeshw-hwik <lee.shw@hwikgo.com>
2026-02-14 23:32:32 +0900

    chore(v1.8.4): Update configs & release notes for final v1.8.4

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
 
     app = QApplication(sys.argv)
     app.setApplicationName("LnxTerm")
-    app.setApplicationVersion("1.8.3")
+    app.setApplicationVersion("1.8.4")
     app.setOrganizationName("LnxTerm")
 
     # 기본 폰트 설정
